8248393cce4156de64e1cecaa59365037fb541a7
Borja Gómez Muñoz <borja@reverscore.com>
2022-05-05 22:36:09 +0200

    Change model training and fine/tunning

--- app/model/model.py
+++ app/model/model.py
@@ -1,25 +1,23 @@
 import tensorflow as tf
-from keras.layers.core import Dense,Flatten
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-from keras.utils.np_utils import to_categorical  
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from keras.utils.np_utils import to_categorical 
 
-resnet_model = Sequential()
-
-pretrained_model= tf.keras.applications.ResNet50(include_top=False,
+pretrained_model= ResNet50(
+                   include_top=False,
                    input_shape=(32,32,3),
-                   pooling='avg',classes=5,
                    weights='imagenet')
+model = pretrained_model.output
+model = GlobalAveragePooling2D()(model)
+model = Dense(1024, activation='relu')(model)
+predictions = Dense(100, activation='softmax')(model)
+
+model = Model(inputs=pretrained_model.input, outputs=predictions)
+
 for layer in pretrained_model.layers:
-        layer.trainable=False
-
-resnet_model.add(pretrained_model)
-
-resnet_model.add(Flatten())
-resnet_model.add(Dense(512, activation='relu'))
-resnet_model.add(Dense(100, activation='softmax'))
-
-resnet_model.summary()
+    layer.trainable = False
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data()
 assert x_train.shape == (50000, 32, 32, 3)
@@ -30,5 +28,5 @@ assert y_test.shape == (10000, 1)
 y_train_onehot = to_categorical(y_train, num_classes=100)
 y_test_onehot = to_categorical(y_test, num_classes=100)
 
-resnet_model.compile(optimizer=Adam(learning_rate=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
-history = resnet_model.fit(x_train, y_train_onehot, validation_data=(x_test, y_test_onehot), epochs=10)
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy'])
+history = model.fit(x_train, y_train_onehot, validation_data=(x_test, y_test_onehot), epochs=10)
